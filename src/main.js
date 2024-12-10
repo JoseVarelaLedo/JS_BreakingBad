1d5ca5e473244ef95647ef57b7db31d91624f2df
Jose Ledo <j.varela@sixtema.es>
2024-12-10 11:39:00 +0100

    switched to Breaking Bad

--- src/main.js
+++ src/main.js
@@ -8,14 +8,13 @@ import { BreakingbadApp } from './breakingbad/breakingbad-app';
 document.querySelector('#app').innerHTML = `
   <div>
   
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+    <a href="https://breakingbadquotes.xyz/" target="_blank">
       <img src="${bbLogo}" class="logo vanilla" alt="Breaking Bad Logo" />
     </a>
     <h1 id="app-title">Htpp App</h1>
     <div class="card">
     
-    </div>
-    
+    </div>    
   </div>
 `;
 
